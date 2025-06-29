ba9d2aaa7d98864e8613e2a9c24d7acc600cde8e
imamhossenbu <imam.cse9.bu@gmail.com>
2025-06-29 20:40:42 +0600

    modify jwt api

--- index.js
+++ index.js
@@ -60,7 +60,7 @@ app.post('/jwt', (req, res) => {
   const cookieOptions = {
     httpOnly: true,
     secure: process.env.NODE_ENV === 'production',
-    sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
+    sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
     maxAge: 5 * 60 * 60 * 1000,
   };
   res.cookie('token', token, cookieOptions).send({ success: true });
