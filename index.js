d672f470d6cea227fe58fca953707d4d09b7b571
imamhossenbu <imam.cse9.bu@gmail.com>
2025-07-08 13:09:21 +0600

    modify

--- index.js
+++ index.js
@@ -16,6 +16,8 @@ app.use(cors({
 
 app.use(express.json());
 
+console.log(process.env.DB_USER);
+
 // MongoDB Setup
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.mc0punk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 const client = new MongoClient(uri, {
